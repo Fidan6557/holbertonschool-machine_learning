7d97d1fb6c656f4bce6b65068d1a14c808e1819c
Fidan <fbbagirova2006@gmail.com>
2026-01-09 13:30:01 +0400

    h

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -6,4 +6,4 @@ import pandas as pd
 def from_numpy(array):
     """ creates a pd.DataFrame from a np.ndarray """
     alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    return pd.DataFrame(array, columns = alphabet[:array.shape[1]])
+    return pd.DataFrame(array, columns=alphabet[:array.shape[1]])
