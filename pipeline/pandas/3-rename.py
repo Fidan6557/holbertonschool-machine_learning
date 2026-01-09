766fd736cd9eedc43d0dee9e9182d2fef51f6224
Fidan <fbbagirova2006@gmail.com>
2026-01-09 17:04:10 +0400

    ed

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Renames the Timestamp column to Datetime, converts it to datetime
+format, and returns a DataFrame with only the Datetime and Close columns.
+"""
+import pandas as pd
+
+
+def rename(df):
+    """Renames the Timestamp column to Datetime, converts it to datetime
+    format, and returns a DataFrame with only the Datetime and Close columns.
+    """
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    return df[['Datetime', 'Close']]
