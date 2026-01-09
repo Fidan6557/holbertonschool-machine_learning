0c47401873eb9e292052b82a3ad2e58d4f233e80
Fidan <fbbagirova2006@gmail.com>
2026-01-09 13:55:15 +0400

    h

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -6,6 +6,6 @@ A functionthat loads data from a file as a pd.DataFrame
 import pandas as pd
 
 
-def from_file(filename, delimiter): 
+def from_file(filename, delimiter):
     """ creates a pd.DataFrame from a file """
     return pd.read_csv(filename, delimiter=delimiter)
