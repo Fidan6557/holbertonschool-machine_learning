fef0f5874e05ce2dc80a726e8c52aad7ad719d62
Fidan <fbbagirova2006@gmail.com>
2026-01-09 17:46:49 +0400

    r

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -3,5 +3,5 @@
 
 
 def slice(df):
-   """Slices the DataFrame every 60 rows and selects specific columns."""
-   return df.loc[:, ['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
+    """Return High, Low, Close, Volume_(BTC) columns every 60 rows."""
+    return df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
