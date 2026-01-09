267734893933fdcfdca6686aa31670225204b1f0
Fidan <fbbagirova2006@gmail.com>
2026-01-09 17:45:08 +0400

    d

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Slices the DataFrame every 60 rows and selects specific columns."""
 
+
 def slice(df):
    """Slices the DataFrame every 60 rows and selects specific columns."""
    return df.loc[:, ['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
