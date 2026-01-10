e36813ec192cf1a3d71e75090bc809df902b5a52
Fidan <fbbagirova2006@gmail.com>
2026-01-10 15:02:05 +0400

    b

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -4,11 +4,11 @@
 
 def fill(df):
     """Filling missing values"""
-    df = df.drop(columns=['Weighted_Price'])
-    df['Close'] = df['Close'].fillna(method='ffill')
-    df[['High', 'Low', 'Open']] =\
-          df[['High', 'Low', 'Open']].fillna(df['Close']).ffill()
-    df[['Volume_(BTC)', 'Volume_(Currency)']] = (
-        df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
-    )
+    df = df.drop(columns=["Weighted_Price"])
+    df['Close'] = df['Close'].ffill()
+    df['High'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
+    df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
     return df
