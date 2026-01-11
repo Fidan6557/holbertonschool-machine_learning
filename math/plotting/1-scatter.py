ad192068b43f7fde2649551356e34934c7bd5835
Fidan <fbbagirova2006@gmail.com>
2026-01-11 17:07:28 +0400

    h

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def scatter():
-    """Scatter plot"""
+    """Plot a scatter graph of men's height vs weight."""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
