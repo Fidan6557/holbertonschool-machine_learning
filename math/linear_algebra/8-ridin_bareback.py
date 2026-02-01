fccad0b31bdfc174ad243e1803d1a00895fc998f
Fidan Bagirova <fidan.bagirova@gmail.com>
2026-02-01 12:08:11 +0000

    fgj

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-"""A function that performs matrix multiplication"""
-
-
 def mat_mul(mat1, mat2):
     """A function that performs matrix multiplication"""
-    if len(mat1) != len(mat2[0]):
+    if not mat1 or not mat2 or len(mat1[0]) != len(mat2):
         return None
-    else:
-        [sum(a * b) for a, b in zip(col,row) for col in zip(*mat2)] for row in mat1]
+
+    return [
+        [
+            sum(a * b for a, b in zip(row, col))
+            for col in zip(*mat2)
+        ]
+        for row in mat1
+    ]
