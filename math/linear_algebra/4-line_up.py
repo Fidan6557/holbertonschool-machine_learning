3bae30ff4615d66c8285ec033677603e9ba023da
Fidan Bagirova <seninemailin@gmail.com>
2026-02-01 09:46:10 +0000

    h

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,4 +4,6 @@
 
 def add_arrays(arr1, arr2):
     """Adds two arrays element-wise """
-    return [a+b for a,b in zip(arr1, arr2)]
+    if len(arr1) != len(arr2):
+        return None
+    return [a+b for a, b in zip(arr1, arr2)]
