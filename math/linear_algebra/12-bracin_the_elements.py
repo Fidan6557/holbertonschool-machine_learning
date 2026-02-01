35362735a26bbd745800863b9f4fb1ecabb28924
Fidan Bagirova <fidan.bagirova@gmail.com>
2026-02-01 12:42:16 +0000

    c

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""A function  that performs element-wise addition, subtraction, multiplication, and division"""
+"""
+    A function  that performs element-wise addition,
+ subtraction, multiplication, and division
+ """
 
 
 def np_elementwise(mat1, mat2):
