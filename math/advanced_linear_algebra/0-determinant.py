0f72507c95534ea22fa25ba72e5a7903a40f7121
Fidan Baghirova <fidan@gmail.com>
2026-02-08 12:50:14 +0000

    <-->

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -16,7 +16,7 @@ def determinant(matrix):
 
     if len(matrix) == 1:
         return matrix[0][0]
-    
+
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
