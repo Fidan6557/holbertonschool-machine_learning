f730bde6c54bf0d5ca90a937cd0cbee1799e5d10
Ad Soyad <email@example.com>
2026-02-15 17:47:17 +0000

    <-->

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,33 +1,37 @@
-binomial.py
-"""a class Exponential that represents an exponential distribution:"""
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
 
 
-class Exponential():
-    """a class Exponential that represents an exponential distribution:"""
-
-    e = 2.7182818285
+class Exponential:
+    """A class that does the trick"""
 
     def __init__(self, data=None, lambtha=1.):
-        self.data = data
+        """A function that does the trick"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = 1 / (sum(data) / len(data))
+            else:
+                self.lambtha = (1 / (sum(data) / len(data)))
 
     def pdf(self, x):
-        """Probability density function"""
+        """Calculates the value of the PDF"""
+        e = 2.7182818285
         if x < 0:
             return 0
-        return self.lambtha * self.e ** (-self.lambtha * x)
+        pdf = self.lambtha * (e ** ((-self.lambtha) * x))
+        return pdf
 
     def cdf(self, x):
-        """Cumulative distribution function"""
+        """Calculates the value of the CDF"""
+        e = 2.7182818285
         if x < 0:
             return 0
-        return 1 - pow(self.e, -self.lambtha*x)
+        cdf = 1 - (e ** ((-self.lambtha) * x))
+        return cdf
