7beda5ec9139caefca8cbab9068b5928c32c4bfa
Fidan <fbbagirova2006@gmail.com>
2026-03-08 22:26:38 +0400

    <-->

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-"""a class that defines a single neuron performing binary classification"""
+
+
+"""Classification"""
+
+
 import numpy as np
 
 
 class Neuron:
-    """class constructor"""
+    """Neuron Class"""
 
     def __init__(self, nx):
-        """initialize neuron"""
+        """Init Function"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -19,45 +23,58 @@ class Neuron:
 
     @property
     def W(self):
-        """getter for W"""
+        """Getter Method"""
         return self.__W
 
     @property
     def b(self):
-        """getter for b"""
+        """Getter Method"""
         return self.__b
 
     @property
     def A(self):
-        """getter for A"""
+        """Getter Method"""
         return self.__A
 
     def forward_prop(self, X):
-        """forward propagation of the neuron"""
+        """Forward Propagation"""
         Z = np.dot(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
 
     def cost(self, Y, A):
-        """cost of the model"""
+        """Cost Function"""
         m = Y.shape[1]
-        cost = - (1 / m) * np.sum(
-            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
-            )
-        return cost
+        J = -1 / m * np.sum((Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)))
+        return J
 
     def evaluate(self, X, Y):
-        """evaluate the neuron’s predictions"""
+        """Evaluate Prediction"""
         A = self.forward_prop(X)
+        pred = np.where(A >= 0.5, 1, 0)
         cost = self.cost(Y, A)
-        prediction = np.where(A >= 0.5, 1, 0)
-        return prediction, cost
+        return pred, cost
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
-        """gradient descent of the neuron"""
+        """Gradient Descent"""
         m = Y.shape[1]
         dZ = A - Y
-        dW = np.dot(dZ, X.T) / m
-        db = np.sum(dZ) / m
-        self.__W -= alpha * dW
-        self.__b -= alpha * db
+        dW = 1 / m * np.dot(dZ, X.T)
+        db = (1/m) * np.sum(dZ)
+        self.__W = self.__W - (alpha * dW)
+        self.__b = self.__b - (alpha * db)
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Train The Model"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+        return self.evaluate(X, Y)
