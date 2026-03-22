ab6aaee294c7edf017c442287019aee8d1747edc
Fidan <fbbagirova2006@gmail.com>
2026-03-22 13:33:05 +0400

    Refactor L2 regularization gradient descent function for improved clarity and structure

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -4,26 +4,24 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates weights via GD with L2 regularization."""    m = Y.shape[1]
+    """Updates weights via GD with L2 regularization."""
     m = Y.shape[1]
-    dZ = cache['A' + str(L)] - Y
- 
-    for layer in range(L, 0, -1):
-        A_prev = cache['A' + str(layer - 1)]
-        W = weights['W' + str(layer)]
-        b = weights['b' + str(layer)]
- 
-        # Compute gradients with L2 regularization term for W
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
- 
-        # Compute dZ for the previous layer (tanh activation)
-        if layer > 1:
-            # Derivative of tanh: 1 - A^2
-            dA_prev = np.matmul(W.T, dZ)
-            dZ = dA_prev * (1 - A_prev ** 2)
- 
-        # Update weights and biases in place
-        weights['W' + str(layer)] = W - alpha * dW
-        weights['b' + str(layer)] = b - alpha * db
- 
+    Wc = weights.copy()
+
+    for i in reversed(range(L)):
+        A = cache["A" + str(i + 1)]
+        if i == L - 1:
+            dZ = cache["A" + str(i + 1)] - Y
+            dW = (np.matmul(cache["A" + str(i)], dZ.T) / m).T
+            dW_L2 = dW + (lambtha / m) * Wc["W" + str(i + 1)]
+            db = np.sum(dZ, axis=1, keepdims=True) / m
+        else:
+            dW2 = np.matmul(Wc["W" + str(i + 2)].T, dZ2)
+            tanh = 1 - (A * A)
+            dZ = dW2 * tanh
+            dW = np.matmul(dZ, cache["A" + str(i)].T) / m
+            dW_L2 = dW + (lambtha / m) * Wc["W" + str(i + 1)]
+            db = np.sum(dZ, axis=1, keepdims=True) / m
+        weights["W" + str(i + 1)] = (Wc["W" + str(i + 1)] - (alpha * dW_L2))
+        weights["b" + str(i + 1)] = Wc["b" + str(i + 1)] - (alpha * db)
+        dZ2 = dZ
