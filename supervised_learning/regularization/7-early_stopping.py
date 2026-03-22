116d20ddd97823c22843854823193ae93c9108d7
Fidan <fbbagirova2006@gmail.com>
2026-03-22 14:09:11 +0400

    Add early stopping function to monitor training progress

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" An early stopping"""
+import tensorflow as tf
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Determines if you should stop training early"""
+    if opt_cost - cost < threshold:
+        count += 1
+    else:
+        count = 0
+
+    return count >= patience, count
