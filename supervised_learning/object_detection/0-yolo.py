dd20938856b2ca6a7a83572bbc750069449d04bc
Fidan6557 <fbbagirova2006@gmail.com>
2026-05-09 12:50:22 +0400

    Fix docstring formatting in Yolo class

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -4,8 +4,7 @@ from tensorflow import keras as K
 
 
 class Yolo:
-    """ a class Yolo that uses the Yolo v3 
-    algorithm to perform object detection """
+    """a class Yolo that uses the Yolo v3 algorithm to perform object detection"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
