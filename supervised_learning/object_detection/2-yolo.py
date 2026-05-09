9f8427577e4d508698b9fd54405c658398d93826
Fidan6557 <fbbagirova2006@gmail.com>
2026-05-09 13:32:08 +0400

    Refactor process_outputs method in Yolo class for improved readability and structure

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -43,39 +43,48 @@ class Yolo():
         Returns: (boxes, box_confidences, box_class_probs)
         """
         boxes = [pred[:, :, :, 0:4] for pred in outputs]
+
         for ipred, pred in enumerate(boxes):
             for grid_h in range(pred.shape[0]):
                 for grid_w in range(pred.shape[1]):
                     bx = ((self.sigmoid(pred[grid_h,
-                                        grid_w, :,
-                                        0]) + grid_w) / pred.shape[1])
+                                            grid_w, :,
+                                            0]) + grid_w) / pred.shape[1])
                     by = ((self.sigmoid(pred[grid_h,
-                                        grid_w, :,
-                                        1]) + grid_h) / pred.shape[0])
-                    anchor_tensor = self.anchors[ipred].astype(float)
-                    anchor_tensor[:, 0] *= \
-                        np.exp(pred[grid_h, grid_w, :,
-                               2]) / self.model.input.shape[1]
-                    anchor_tensor[:, 1] *= \
-                        np.exp(pred[grid_h, grid_w, :,
-                               3]) / self.model.input.shape[2]
-                    pred[grid_h, grid_w, :, 0] = \
-                        (bx - (anchor_tensor[:, 0] / 2)) * \
-                        image_size[1]
-                    pred[grid_h, grid_w, :, 1] = \
-                        (by - (anchor_tensor[:, 1] / 2)) * \
-                        image_size[0]
-                    pred[grid_h, grid_w, :, 2] = \
-                        (bx + (anchor_tensor[:, 0] / 2)) * \
-                        image_size[1]
-                    pred[grid_h, grid_w, :, 3] = \
-                        (by + (anchor_tensor[:, 1] / 2)) * \
-                        image_size[0]
+                                            grid_w, :,
+                                            1]) + grid_h) / pred.shape[0])
 
-        box_confidences = [self.sigmoid(pred[:, :, :,
-                                        4:5]) for pred in outputs]
-        box_class_probs = [self.sigmoid(pred[:, :, :,
-                                        5:]) for pred in outputs]
+                    anchor_tensor = self.anchors[ipred].astype(float)
+
+                    anchor_tensor[:, 0] *= (
+                        np.exp(pred[grid_h, grid_w, :, 2]) /
+                        self.model.input.shape[1]
+                    )
+                    anchor_tensor[:, 1] *= (
+                        np.exp(pred[grid_h, grid_w, :, 3]) /
+                        self.model.input.shape[2]
+                    )
+
+                    pred[grid_h, grid_w, :, 0] = (
+                        bx - (anchor_tensor[:, 0] / 2)
+                    ) * image_size[1]
+                    pred[grid_h, grid_w, :, 1] = (
+                        by - (anchor_tensor[:, 1] / 2)
+                    ) * image_size[0]
+                    pred[grid_h, grid_w, :, 2] = (
+                        bx + (anchor_tensor[:, 0] / 2)
+                    ) * image_size[1]
+                    pred[grid_h, grid_w, :, 3] = (
+                        by + (anchor_tensor[:, 1] / 2)
+                    ) * image_size[0]
+
+        box_confidences = [
+            self.sigmoid(pred[:, :, :, 4:5]) for pred in outputs
+        ]
+        box_class_probs = [
+            self.sigmoid(pred[:, :, :, 5:]) for pred in outputs
+        ]
+
         return boxes, box_confidences, box_class_probs
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
@@ -95,13 +104,14 @@ class Yolo():
         all_classes = []
         all_scores = []
 
-        for boxes_i, confidences_i, probs_i in zip(boxes,
-                                                    box_confidences,
-                                                    box_class_probs):
+        for boxes_i, confidences_i, probs_i in zip(
+                boxes, box_confidences, box_class_probs):
             box_scores = confidences_i * probs_i
             box_classes = np.argmax(box_scores, axis=-1)
             box_class_scores = np.max(box_scores, axis=-1)
+
             mask = box_class_scores >= self.class_t
+
             all_boxes.append(boxes_i[mask])
             all_classes.append(box_classes[mask])
             all_scores.append(box_class_scores[mask])
