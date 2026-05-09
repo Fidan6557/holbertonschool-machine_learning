eb903b117e666c77dbfb7ba70bb596297e5b67c3
Fidan6557 <fbbagirova2006@gmail.com>
2026-05-09 13:27:45 +0400

    Refactor docstrings and comments for clarity in Yolo class

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-"""Object detection with YOLOv3"""
+"""Object Detection"""
 from tensorflow import keras as K
+import numpy as np
 
 
 class Yolo:
-    """a class Yolo that uses the Yolo v3 algorithm
-            to perform object detection"""
-    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        # Load Darknet (converted) Keras model
+    """Class of Yolo"""
+    def __init__(self, model_path, classes_path,
+                 class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
-
-        # Load class names
         with open(classes_path, 'r') as f:
-            self.class_names = [line.strip() for line in f.readlines()]
-
-        # Detection thresholds
+            self.class_names = [line.strip()
+                                for line in f.readlines()]
         self.class_t = class_t
         self.nms_t = nms_t
-
-        # YOLO anchors
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """process outputs"""
+        """Process Outputs"""
         boxes = []
         box_confidences = []
         box_class_probs = []
@@ -40,8 +35,8 @@ class Yolo:
             box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
 
             cx = np.arange(grid_w).reshape(1, grid_w, 1)
-            cy = np.arange(grid_h).reshape(grid_h, 1, 1)\
-            
+            cy = np.arange(grid_h).reshape(grid_h, 1, 1)
+
             bx = (1 / (1 + np.exp(-tx)) + cx) / grid_w
             by = (1 / (1 + np.exp(-ty)) + cy) / grid_h
 
