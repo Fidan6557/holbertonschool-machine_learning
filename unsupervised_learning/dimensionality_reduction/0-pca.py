0ec6b8dc84263774db13c2206865f140de2d62ae
Fidan6557 <fbbagirova2006@gmail.com>
2026-05-17 20:29:15 +0400

    Refactor PCA implementation for clarity and efficiency

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""Principal Component Analysis (PCA)"""
+"""A module that does the trick"""
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """PCA"""
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
-
-    squared_singular_values = S**2
-    total_variance = np.sum(squared_singular_values)
-    cumulative_variance_ratio = np.cumsum(squared_singular_values) / total_variance
-    nd = np.argmax(cumulative_variance_ratio >= var) + 1
-    W = Vt[:nd].T
-    return W
+    """A function that does the trick"""
+    u, Sigma, vh = np.linalg.svd(X, full_matrices=False)
+    cumulative_var = np.cumsum(Sigma) / np.sum(Sigma)
+    r = (np.argwhere(cumulative_var >= var))[0, 0]
+    w = vh.T
+    wr = w[:, :r + 1]
+    return wr
